e5696bd56bdf8af8ccaecfdc4c42d04b4e81331a
eshan1799 <eshan1799@gmail.com>
2020-10-29 13:39:02 +0000

    add HomePage + Error404 routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { HomePage, Error404 } from './Containers/index/index';
 import "./styles/App.css";
+
 class App extends React.Component {
-  state = {};
   render() {
     return (
       <>
-        <h1>New React App</h1>
-        <h2>Happy Coding</h2>
+        <div className="App">
+          <h1>New React App</h1>
+          <h2>Happy Coding</h2>
+
+          <Switch>
+            <Route exact path='/' component={ HomePage } />
+            <Route component={ Error404 } />
+          </Switch>
+      </div>
       </>
     );
   }
