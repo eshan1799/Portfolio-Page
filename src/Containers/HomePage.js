cdcbefec90d6a564f471d08965b008514f7b2365
eshan1799 <eshan1799@gmail.com>
2020-10-29 13:45:07 +0000

    create HomePage container

---
+++ src/Containers/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Eshan Chatwal</h1>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
