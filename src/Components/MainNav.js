9e529d33e6127eef039b685f195e6f4e2d19975b
eshan1799 <eshan1799@gmail.com>
2020-11-04 15:38:12 +0000

    create main navbar component (fixed to top)

---
+++ src/Components/MainNav.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Scrollspy from 'react-scrollspy';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { Nav, Navbar } from "react-bootstrap";
+
+class MainNav extends React.Component {
+    render() {
+        return (
+            <>
+            <Navbar id="navbar" fixed="top">
+                    <Scrollspy 
+                    className="navFlex"
+                    items={ ['top', 'about', 'skills', 'projects', 'contact'] } 
+                    currentClassName="isCurrent">
+                <Navbar.Brand>
+                    <AnchorLink className="anchor" href="#top">
+                        EC
+                    </AnchorLink>
+                </Navbar.Brand>
+                    <Nav.Link id="mid" className="navlinks">
+                        <AnchorLink href="#about">
+                            About
+                        </AnchorLink>
+                    </Nav.Link>
+                    <Nav.Link className="navlinks">
+                        <AnchorLink href="#skills">
+                            Skills
+                        </AnchorLink>
+                    </Nav.Link>
+                    <Nav.Link className="navlinks">
+                        <AnchorLink href="#projects">
+                            Projects
+                        </AnchorLink>
+                    </Nav.Link>
+                    <Nav.Link className="navlinks">
+                        <AnchorLink href="#contact">
+                            Contact
+                        </AnchorLink>
+                    </Nav.Link>
+                </Scrollspy>
+            </Navbar>
+            </>
+        )
+    }
+}
+
+export default MainNav;
