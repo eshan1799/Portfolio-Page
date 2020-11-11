ec249a402a53b8bc1b6f416ee9d5f7957e9ebc65
eshan1799 <eshan1799@gmail.com>
2020-11-11 11:48:51 +0000

    remove bootstrap container, rows + columns

--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { MainNav, SideNav, Landing, About, Skills, Projects, Contact } from '../Components/index/index';
+import { MainNav, SideNav, Landing, About, Skills, Projects, Contact, Footer } from '../Components/index/index';
 
 class HomePage extends React.Component {
     render() {
         return (
             <>
-            <MainNav />
-            <Container>
-                <Row>
-                    <Col>
-                    <SideNav />
-                    </Col>
-                    <Col xs={12}>
-                        <Landing />
-                        <About />
-                        <Skills />
-                        <Projects />
-                        <Contact />
-                    </Col>
-                </Row>
-            </Container>
+                <MainNav />
+                <SideNav />
+                <Landing/>
+                <About />
+                <Skills />
+                <Projects />
+                <Contact />
+                <Footer />
             </>
         )
     }
