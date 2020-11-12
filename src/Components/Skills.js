efe49cdd037f83b1642bc3343fa2fbc82a6910b7
eshan1799 <eshan1799@gmail.com>
2020-11-12 00:15:03 +0000

    import bootstrap cards + create 3 card layout

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card, CardGroup } from "react-bootstrap";
 import { AnimatedBg, Transition } from 'scroll-background';
 
 class Skills extends React.Component {
@@ -14,9 +15,44 @@ class Skills extends React.Component {
                             <h2>
                                 Skills
                             </h2>
-                            <p>
+                            {/* <p>
                                 What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                            </p>
+                            </p> */}
+                            <CardGroup>
+                                <Card>
+                                    <Card.Body className="centreFlexColumn">
+                                        <Card.Title>
+                                            Frontend
+                                        </Card.Title>
+                                        <Card.Text>
+                                            This is a wider card with supporting text below as a natural lead-in to
+                                            additional content. This content is a little bit longer.
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                                <Card>
+                                    <Card.Body className="centreFlexColumn">
+                                        <Card.Title>
+                                            Backend
+                                        </Card.Title>
+                                        <Card.Text>
+                                            This is a wider card with supporting text below as a natural lead-in to
+                                            additional content. This content is a little bit longer.
+                                    </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                                <Card>
+                                    <Card.Body className="centreFlexColumn">
+                                        <Card.Title>
+                                            Other
+                                        </Card.Title>
+                                        <Card.Text>
+                                            This is a wider card with supporting text below as a natural lead-in to
+                                            additional content. This content is a little bit longer.
+                                    </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </CardGroup>
                         </section>
                     </Transition>
                 </AnimatedBg>
