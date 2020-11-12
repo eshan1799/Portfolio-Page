dc223a13093bf23d96e999bdabdb426d88dbafeb
eshan1799 <eshan1799@gmail.com>
2020-11-12 02:27:30 +0000

    remove id from first p

--- src/Components/About.js
+++ src/Components/About.js
@@ -17,7 +17,7 @@ class About extends React.Component {
                             <p id="morePadding" className="centreP">
                                 I am an Economics graduate from City, University of London and have always possessed a growing zeal for IT and technology. I was first introduced to programming in 2013 - after deciding to found my own tech review site and complementary Android application, and subsequently have continued to broaden my technical skills and knowledge of the highly dynamic sector.
                             </p>
-                            <p className="centreP">
+                            <p id="paraSpace" className="centreP">
                                 With the constant inception of new and upcoming disruptive technologies, it is a particularly exciting time to be a tech enthusiast. This passion led me to enrol in a software engineering bootcamp, which I recently completed. During this time, I thoroughly enjoyed learning to use new software and programming languages and thereafter have continued to develop my technical skillset.
                             </p>
                         </section>
