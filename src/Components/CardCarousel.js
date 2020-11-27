cc34451c6c68c45af399b95be7df53585ad467e3
eshan1799 <eshan1799@gmail.com>
2020-11-27 22:41:10 +0000

    add 3rd slide (Habitapp)

--- src/Components/CardCarousel.js
+++ src/Components/CardCarousel.js
@@ -20,7 +20,7 @@ export default function CardCarousel() {
       <Slider {...settings}>
         
         {/* 3X */}
-        <div className="projectBlocks">
+        <div id="blue" className="projectBlocks">
           <section className="projects">
             <img className="projectImage" src="./img/Projects/3X.png"></img>
             <div className="projectInfo">
@@ -63,33 +63,33 @@ export default function CardCarousel() {
         </div>
 
         {/* Trivia Boss */}
-        <div className="projectBlocks">
+        <div id="pink" className="projectBlocks">
           <section className="projects">
-            <img className="projectImage" src="./img/Projects/3X.png"></img>
+            <img className="projectImage" src="./img/Projects/TriviaBoss.png"></img>
             <div className="projectInfo">
               <h3 className="projectTitle">
                 Trivia Boss
               </h3>
               <p className="projectDescription">
-                A web-based virtual trading app, to experience market trading without the risk.
+                A multiplayer quiz, where players can test their knowledge of different topics.
               </p>
               <ul>
                 <li className="projectPointers">
                   <Arrow />
                   <span className="bulletPointArrow">
-                    Login page
+                    Built with React.js
                   </span>
                 </li>
                 <li className="projectPointers">
                   <Arrow />
                   <span className="bulletPointArrow">
-                    Secure server routes
+                    Retrieve questions from an external API
                   </span>
                 </li>
                 <li className="projectPointers">
                   <Arrow />
                   <span className="bulletPointArrow">
-                    Real-time market data
+                    Choose topics and difficulty
                   </span>
                 </li>
               </ul>
@@ -104,9 +104,47 @@ export default function CardCarousel() {
             </div>
           </section>
         </div>
-        <div>
-          {/* Slide 3 */}
+        
+        {/* Habitapp */}
+        <div id="neonBlue" className="projectBlocks">
+          <section className="projects">
+            <img className="projectImage" src="./img/Projects/HabitApp.png"></img>
+            <div className="projectInfo">
+              <h3 className="projectTitle">
+                Habitapp
+              </h3>
+              <p className="projectDescription">
+                A habit-tracking web application, that allows users to manage their habits and promotes self growth.
+              </p>
+              <ul>
+                <li className="projectPointers">
+                  <Arrow />
+                  <span className="bulletPointArrow">
+                    Secure login and register
+                  </span>
+                </li>
+                <li className="projectPointers">
+                  <Arrow />
+                  <span className="bulletPointArrow">
+                    View and add habits in your dashboard
+                  </span>
+                </li>
+                <li className="projectPointers">
+                  <Arrow />
+                  <span className="bulletPointArrow">
+                    Calendar view to show more habit info
+                  </span>
+                </li>
+              </ul>
+              <div className="projectButtons">
+                <a href="https://github.com/eshan1799/Habitapp" className="buttons" target="_blank">
+                  Github
+                </a>
+              </div>
+            </div>
+          </section>
         </div>
+
         <div>
           {/* Slide 4 */}
         </div>
