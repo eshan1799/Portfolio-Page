94c65e0805e504d579bbdef74c88a3f8e6d5b3bf
eshan1799 <eshan1799@gmail.com>
2022-04-04 14:52:30 +0100

    update job title

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -15,7 +15,7 @@ class Landing extends React.Component {
                         Eshan Chatwal
                     </h1>
                     <p id="role">
-                        Junior DevOps Engineer
+                        Associate DevOps Engineer
                     </p>
                 </div>
             </section>
