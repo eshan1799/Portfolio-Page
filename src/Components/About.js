ea543f4dcb1ec1d2df15bbd23852b68ea06c3bec
eshan1799 <eshan1799@gmail.com>
2022-04-11 12:56:19 +0100

    update About Me

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,10 +15,10 @@ class About extends React.Component {
                                 About
                             </h2>
                             <p className="centreP">
-                                I am an Associate DevOps/Platform engineer at Capgemini and have always possessed a growing zeal for IT and technology. I was first introduced to programming in 2013 - after deciding to found my own tech review site and complementary Android application. I have since continued to broaden my technical skills and knowledge of the highly dynamic sector.
+                            I am an Associate DevOps/Platform engineer at Capgemini and have always possessed a growing zeal for IT and technology. I was first introduced to programming in 2013 - after deciding to found my own tech review site and complementary Android application, and have since continued to broaden my knowledge of the highly dynamic sector.
                             </p>
                             <p id="paraSpace" className="centreP">
-                                This passion led me to enrol in a software engineering bootcamp, shortly after graduating in Economics from City, University of London. Subsequently, I have worked as a junior full-stack engineer and currently as a DevOps engineer. I have thoroughly enjoyed learning to use new software and programming languages, and currently have certifications in AWS (Certified Cloud Practitioner) and Terraform (HashiCorp Certified: Terraform Associate).
+                                This passion led me to enrol in a software engineering bootcamp, shortly after graduating in Economics from City, University of London. Soon after that, I worked as a junior full-stack engineer and now currently as a DevOps engineer. I thoroughly enjoy learning to use new software and programming languages, and currently have certifications in AWS (Certified Cloud Practitioner) and Terraform (HashiCorp Certified: Terraform Associate).
                             </p>
                         </section>
                     </Transition>
