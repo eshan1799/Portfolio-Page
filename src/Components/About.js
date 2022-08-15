0fd7a8763ec905e9e8c47dcbba7a8793db26254f
eshan1799 <eshan1799@gmail.com>
2022-08-15 12:01:21 +0100

    add a mention of AWS dev cert

--- src/Components/About.js
+++ src/Components/About.js
@@ -18,7 +18,7 @@ class About extends React.Component {
                             I am an Associate DevOps/Platform engineer at Capgemini and have always possessed a growing zeal for IT and technology. I was first introduced to programming in 2013 - after deciding to found my own tech review site and complementary Android application, and have since continued to broaden my knowledge of the highly dynamic sector.
                             </p>
                             <p id="paraSpace" className="centreP">
-                                This passion led me to enrol in a software engineering bootcamp, shortly after graduating in Economics from City, University of London. Soon after that, I worked as a junior full-stack engineer and now currently as a DevOps engineer. I thoroughly enjoy learning to use new software and programming languages, and currently have certifications in AWS (Certified Cloud Practitioner) and Terraform (HashiCorp Certified: Terraform Associate).
+                                This passion led me to enrol in a software engineering bootcamp, shortly after graduating in Economics from City, University of London. Soon after that, I worked as a junior full-stack engineer and now currently as a DevOps engineer. I thoroughly enjoy learning to use new software and programming languages, and currently have certifications in AWS (Certified AWS Developer - Associate) and Terraform (HashiCorp Certified: Terraform Associate).
                             </p>
                         </section>
                     </Transition>
